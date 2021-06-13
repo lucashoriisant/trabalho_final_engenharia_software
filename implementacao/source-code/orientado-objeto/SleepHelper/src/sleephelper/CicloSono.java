82e39ec6e35c40b1d8e326265ec1f57363b18d82
Lucas Horii <luck201211@hotmail.com>
2021-06-13 16:26:24 -0300

    Adicionado projeto java

---
+++ implementacao/source-code/orientado-objeto/SleepHelper/src/sleephelper/CicloSono.java
@@ -0,0 +1,16 @@
+package sleephelper;
+public class CicloSono {
+    
+    
+    public void estadoAtual(){
+    
+}
+    
+    public void iniciarCiclo(){
+        
+    }
+    
+    public void terminarCiclo(){
+        
+    }
+}
