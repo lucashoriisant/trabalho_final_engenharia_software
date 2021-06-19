92c927cdb1d38104d43baf9e799d7e173eaa4012
Lucas Horii dos Santos <67847769+lucashoriisant@users.noreply.github.com>
2021-06-18 23:42:10 -0300

    Adicionado métodos

--- implementacao/source-code/orientado-objeto/SleepHelper/src/sleephelper/CicloSono.java
+++ implementacao/source-code/orientado-objeto/SleepHelper/src/sleephelper/CicloSono.java
@@ -3,10 +3,15 @@ public class CicloSono {
     
     private String confirmacao;
     private boolean execucao;
+    private boolean status;
+
+    CicloSono() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     public void estadoAtual(){
-        System.out.println("Deseja realmente iniciar o ciclo? " + getConfirmacao());
+        System.out.println("Deseja realmente iniciar o ciclo? " + this.getConfirmacao());
     
 }
     
@@ -16,47 +21,62 @@ public class CicloSono {
             System.out.println("Ciclo iniciado!");
         }else
             System.out.println("O ciclo não foi iniciado!");
-        
     }
     
     public void terminarCiclo(){
         
     }
     
-    public void status (){
-        if (this.getStatus() == false){
-            System.out.println("Pode abrir a conta");
-        } else{
-            System.out.println("A conta já está aberta");
-        }  
-        
-    }
-
-    /**
-     * @return the confirmacao
-     */
     public String getConfirmacao() {
         return confirmacao;
     }
 
-    /**
-     * @param confirmacao the confirmacao to set
-     */
     public void setConfirmacao(String confirmacao) {
         this.confirmacao = confirmacao;
     }
 
-    /**
-     * @return the execucao
-     */
-    public boolean isExecucao() {
+    public boolean getExecucao() {
+        return setExecucao();
+    }
+    
+    public boolean setExecucao() {
         return execucao;
     }
-
-    /**
-     * @param execucao the execucao to set
-     */
+        
     public void setExecucao(boolean execucao) {
         this.execucao = execucao;
     }
+    
+    public boolean getStatus() {
+        return this.setStatus();
+    }
+    
+      public boolean setStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean s) {
+        this.status = s;
+    } 
+    
+    public CicloSono(String confirmacao, boolean status) { //Este é o método construtor
+        this.setConfirmacao ("S");
+        this.setStatus (false);
+    }
+
+   
+
+
+   
+  
+
+  
+    /*private void setConfirmacao(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+
+    void estadoatual() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
 }
--- implementacao/source-code/orientado-objeto/SleepHelper/src/sleephelper/SleepHelper.java
+++ implementacao/source-code/orientado-objeto/SleepHelper/src/sleephelper/SleepHelper.java
@@ -1,8 +1,12 @@
 package sleephelper;
 public class SleepHelper {
-    
     public static void main(String[] args) {
-       
+      CicloSono cs = new CicloSono();
+      
+      cs.setConfirmacao("S");
+      
+      cs.estadoatual();
     }
-    
+
+     
 }
